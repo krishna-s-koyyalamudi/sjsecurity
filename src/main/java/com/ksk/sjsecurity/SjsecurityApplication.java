ec5d1e19b345de84d2db3c177eacd458d4b2a8cd
Krishna-Koyyalamudi <60024842+Krishna-Koyyalamudi@users.noreply.github.com>
2024-08-31 12:48:28 -0500

    Initial Commit

---
+++ src/main/java/com/ksk/sjsecurity/SjsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ksk.sjsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksk/sjsecurity/SjsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksk.sjsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
